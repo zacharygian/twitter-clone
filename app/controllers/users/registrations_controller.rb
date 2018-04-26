d22b13f19fa13e5251f9e9c7d8aa4caf18b3bc92
Zachary Gian <z.gian12@eabjm.org>
2018-04-26 15:14:20 +0900

    user can now change password and pw is required when changing user info

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -49,9 +49,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
     devise_parameter_sanitizer.permit(:account_update, keys: [:username, :first_name, :last_name, :age, :biography, :email, :password, :current_password,])
   end
 
-  def update_resource(resource, params)
-    resource.update_without_password(params)
-  end
+  # def update_resource(resource, params)
+  #   resource.update_without_password(params)
+  # end
 
   # The path used after sign up.
   # def after_sign_up_path_for(resource)
